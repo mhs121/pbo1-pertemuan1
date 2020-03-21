ae359d3b23c0c506306e6d8403a77353bac1d93d
mhs121 <muhammadhelmi.septiawan@gmail.com>
2020-03-21 17:25:05 +0800

    hello git dan java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello{
- public static void main (string[] args){
-  system.out.println("hello word !!");
+ public static void main (String [] args){
+  System.out.println("hello Git !!");
+  System.out.println("hello Java !!");
  }
 }
